17224f3eea13d1499053142201100b1a6dc5a1b9
Dale Seo <dale.seo@gmail.com>
2025-09-29 21:07:29 -0400

    remove search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import pagefind from "astro-pagefind";
 import og from "astro-og";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), pagefind(), og()],
+  integrations: [sitemap(), og()],
   output: "static",
   site: "https://www.ajaedale.com",
 });
