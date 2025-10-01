c7bbf3e7f0cc3563b41d51b53c5a2d27d515f32c
Dale Seo <dale.seo@gmail.com>
2025-09-30 22:33:33 -0400

    redesign

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import og from "astro-og";
 import pagefind from "astro-pagefind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap(), og(), pagefind()],
   output: "static",
   site: "https://www.ajaedale.com",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
